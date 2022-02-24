38bf8402ee912a677a45803aa29619db8acbf593
Haoran Jia-PC <21211140001@m.fudan.edu.cn>
2022-02-24 16:20:35 +0800

    寒假前

--- ZS_Aorta.py
+++ ZS_Aorta.py
@@ -31,42 +31,69 @@ EXCEL_COLUMNS = ["index", "f1", "f2", "f3", "Exception", "ct_name", "ct", "seg",
 
 
 class PatientProcessor(object):
+    """
+    对一个患者的主文件夹下的所有图像进行处理
+    分别读取CT、各种分割的DICOM文件序列，并生成nii文件
+    """
     def __init__(self, folder):
-        self.folder = folder
-        self.folder_write = None
-        self.create_folder_write()
-        self.processor_ct = None
+        self.folder = folder    # 患者文件夹地址
+        self.folder_write = None    # 保存提取的nii文件夹的地址
+        self.create_folder_write()  # 创建保存nii的文件夹的地址
+        self.processor_ct = None    #
 
-    def Execute(self, info):
+    def Execute_manually(self, ct_folder):
+        self.load_ct(ct_folder)
+        n_slice = []
+        n_slice.append(self.save_ct())
+
+        for seg_type in SEG_FOLDER:
+            if SEG_FOLDER[seg_type] in os.listdir(self.folder):
+                n_slice.append(self.save_other(seg_type))
+
+        # 输出信息
+        print(ct_folder, end="\t")
+        for i in range(len(n_slice)):
+            print(", ".join([str(n) for n in n_slice[i]]), end="\t")
+
+    def Execute(self, info=None):
         # 保存路径信息
         path_list = self.folder.split('\\')
-        info.loc[len(info), "f1":"f3"] = [path_list[2], path_list[3], path_list[4]]
+        if info is not None:
+            info.loc[len(info), "f1":"f3"] = [path_list[2], path_list[3], path_list[4]]
         # 全部的子文件夹
         sub_folders = os.listdir(self.folder)
         # 寻找ct的子文件夹
         ct_folder = None
         for sub_folder in sub_folders:
-            if re.search(r"CTA 1.0 CE|0.75 *B2|Recon 2_ CTA", sub_folder):
+            if re.search(r"^CTA 1.0 CE$|0.75 *B2|^Recon 2_ CTA$", sub_folder):
                 ct_folder = sub_folder
 
+        # 如果早不到满足格式的CT文件夹，则保存错误信息
         if ct_folder is None:
-            info.loc[len(info) - 1, "Exception"] = "No Expected CT"
+            if info is not None:
+                info.loc[len(info) - 1, "Exception"] = "No Expected CT"
         else:
-            # 读取并保存CT和存在的分割
+            # 有CT文件夹，读取并保存CT和存在的分割
             self.load_ct(ct_folder)
-            n_list = self.save_ct()
-            info.loc[len(info) - 1, "ct_name"] = ct_folder
-            info.loc[len(info) - 1, "ct"] = ", ".join([str(n) for n in n_list])
+            # 将CT文件夹下的序列数量保存在n_list中，并记录
+            n_series_files_list = self.save_ct()
+            if info is not None:
+                info.loc[len(info) - 1, "ct_name"] = ct_folder
+                info.loc[len(info) - 1, "ct"] = ", ".join([str(n) for n in n_series_files_list])
+
             seg_n = 0  # 保存分割图像的数量
             for seg_type in SEG_FOLDER:
                 if SEG_FOLDER[seg_type] in sub_folders:
-                    n_list = self.save_other(seg_type)
-                    seg_n += 1
-                    if len(n_list) != 2:
-                        info.loc[len(info) - 1, "Exception"] = "Too many Seg"
-                    info.loc[len(info) - 1, seg_type] = ", ".join([str(n) for n in n_list])
-            if seg_n not in [1, 6]:
-                info.loc[len(info) - 1, "Exception"] = "Miss Seg"
+                    n_series_files_list = self.save_other(seg_type)
+                    if info is not None:
+                        seg_n += 1
+                        if len(n_series_files_list) != 2:
+                            info.loc[len(info) - 1, "Exception"] = "Too many Seg"
+                        info.loc[len(info) - 1, seg_type] = ", ".join([str(n) for n in n_series_files_list])
+
+            if info is not None:
+                if seg_n not in [1, 6]:
+                    info.loc[len(info) - 1, "Exception"] = "Miss Seg"
 
         return info
 
@@ -231,7 +258,8 @@ def loop(f0_path, isRefill=True):
                                     info = patient.Execute(info)
                                     info.to_excel("dataset\\info.xlsx")
                                 except:
-                                    info.loc[len(info), "f1"] = f1
+                                    # 很奇怪，创造了两行，f1的时候就要减一
+                                    info.loc[len(info) - 1, "f1"] = f1
                                     info.loc[len(info) - 1, "f2"] = f2
                                     info.loc[len(info) - 1, "f3"] = f3
                                     info.loc[len(info) - 1, "Exception"] = "Load Data Error"
@@ -241,17 +269,40 @@ def loop(f0_path, isRefill=True):
                             n_loop += 1
 
 
-def check_patient(patient_folder):
-    p = PatientProcessor(patient_folder)
-    p.lo
-    ct_processor = DCMSerieProcessor(os.path.join(patient_folder, "CTA 1.0 CE"))
-    ct_processor.read()
-    ct_processor.basic_info()
-    time.sleep(0.1)
+def fix_load_data_error():
+    """
+    执行loop循环的时候，判断ct文件夹的正则规则不完善，选到了名为”Vitrea Snapshot_ CTA 1.0 CE“的文件夹导致出错
+    已改正PatientProcessor.Execute()函数，若彻底重新调用loop，无需本函数
+    本函数用改正的Execute函数将”Load Data Error“的数据重新读取
+    :return:
+    """
+    # 读取信息，创建一个待修改的list
+    info_origin = pd.read_excel("dataset/info.xlsx", index_col=[0])
+    info_origin = info_origin[info_origin.Exception == "Load Data Error"]
+    folder_list = []
+    for index, s in info_origin.iterrows():
+        folder = "F:\\ZS_Aorta\\" + str(s["f1"]) + "\\" + s["f2"] + "\\" + s["f3"]
+        folder_list.append((index, folder))
+
+    # 对这个list进行循环，一一修改
+    for index, folder in folder_list:
+        info = pd.read_excel("dataset/info.xlsx", index_col=[0])
+        print(f"index={index}")
+        try:
+            temp_info = pd.DataFrame(columns=EXCEL_COLUMNS)
+            patient = PatientProcessor(folder)
+            temp_info = patient.Execute(temp_info)
+            info.loc[index] = temp_info.loc[0]
+        except:
+            print("又出错了。。。")
+            info.loc[index, "Exception"] = "Load Data Error 2"
+
+        info.to_excel("dataset\\info.xlsx")
 
 
 if __name__ == '__main__':
     # loop("F:\\ZS_Aorta", isRefill=False)
-    check_patient("F:\\ZS_Aorta\\20201224\\ZS11087592_ZS0002168998\\WholeBody CTA")
+    # fix_load_data_error()
 
-    time.sleep(0.1)
+    patient = PatientProcessor(folder="F:\\ZS_Aorta\\20201215\\ZS09021838_ZS0000050828\\PA")
+    patient.Execute_manually(ct_folder="Recon 2_")
